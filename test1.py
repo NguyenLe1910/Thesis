0ac673261c00b8ce409651e909fd00d8851228ac
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-11 03:54:48 +0700

    testing

--- test1.py
+++ test1.py
@@ -39,3 +39,17 @@ try:
 except:
     pass
 time.sleep(0.1)
+
+# Arm
+# master.arducopter_arm() or:
+master.mav.command_long_send(
+    master.target_system,
+    master.target_component,
+    mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+    0,
+    1, 0, 0, 0, 0, 0, 0)
+
+# wait until arming confirmed (can manually check with master.motors_armed())
+print("Waiting for the vehicle to arm")
+master.motors_armed_wait()
+print('Armed!')
