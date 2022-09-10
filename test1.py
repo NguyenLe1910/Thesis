c22d5230b2e6f65fd56a63be1392a6ff1e55a995
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-11 04:26:08 +0700

    testing

--- test1.py
+++ test1.py
@@ -20,17 +20,18 @@ def wait_conn():
     print('Connected')    
 
 def force_arm():
-    print("Force the vehicle to arm")
-
     master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,0,1, 21196, 0, 0, 0, 0, 0)
-
     #msg = master.recv_match(type="COMMAND_ACK",blocking=True)
     #print(msg)
+    try:
+        # wait until arming confirmed (can manually check with master.motors_armed())
+        print("Force the vehicle to arm")
+        master.motors_armed_wait()
+        timestamp = the_connection.time_since(motors_armed_wait)
+        print('Armed!')
+    except:
+        print('Cannot Force Arm')
 
-    # wait until arming confirmed (can manually check with master.motors_armed())
-    print("Waiting for the vehicle to force arm")
-    master.motors_armed_wait()
-    print('Armed!')
 
 
 master = mavutil.mavlink_connection('/dev/serial0',baud=916200)
