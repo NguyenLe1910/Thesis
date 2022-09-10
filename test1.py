86669c1d7ac41d37ce30cbd5beb2f707cb359a6c
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-11 04:42:44 +0700

    testing

--- test1.py
+++ test1.py
@@ -21,8 +21,8 @@ def wait_conn():
 
 def force_arm():
     master.mav.command_long_send(master.target_system,master.target_component,mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,0,1, 0, 0, 0, 0, 0, 0)
-    msg = master.recv_match(type="COMMAND_ACK",blocking=True)
-    print(msg)
+    ack_msg = master.recv_match(type="COMMAND_ACK",blocking=True)
+    print(ack_msg)
     try:
         # wait until arming confirmed (can manually check with master.motors_armed())
         print("Force the vehicle to arm")
@@ -37,4 +37,25 @@ def force_arm():
 master = mavutil.mavlink_connection('/dev/serial0',baud=916200)
 wait_conn()
 print("Heartbeat from system (system %u component %u)" % (master.target_system, master.target_component))
+# Choose a mode
+mode = 'STABILIZE'
+
+# Check if mode is available
+if mode not in master.mode_mapping():
+    print('Unknown mode : {}'.format(mode))
+    print('Try:', list(master.mode_mapping().keys()))
+    sys.exit(1)
+
+# Get mode ID
+mode_id = master.mode_mapping()[mode]
+# Set new mode
+# master.mav.command_long_send(
+#    master.target_system, master.target_component,
+#    mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0,
+#    0, mode_id, 0, 0, 0, 0, 0) or:
+# master.set_mode(mode_id) or:
+master.mav.set_mode_send(
+    master.target_system,
+    mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+    mode_id)
 force_arm()
