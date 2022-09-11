10e3d6d5b3766b0a882868a6365e1a8210589e57
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-12 03:34:56 +0700

    testing

--- stream-video-pi-cam.py
+++ stream-video-pi-cam.py
@@ -45,7 +45,15 @@ class StreamingOutput(object):
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
+        if self.path.find('Arming') > -1:
+            content = PAGE.encode('utf-8')
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(content))
+            #do whatever you want
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == '/':
             self.send_response(301)
             self.send_header('Location', '/index.html')
             self.end_headers()
@@ -78,34 +86,6 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                 logging.warning(
                     'Removed streaming client %s: %s',
                     self.client_address, str(e))
-        elif self.path.find('Arming') > -1:
-            content = PAGE.encode('utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
-            #do whatever you want
-            self.end_headers()
-            self.wfile.write(content)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))          
         else:
             self.send_error(404)
             self.end_headers()
