0129db8e9f700f7104bcf17ce197b299a86e2a9b
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-12 01:41:56 +0700

    testing

--- stream-video-pi-cam.py
+++ stream-video-pi-cam.py
@@ -16,7 +16,7 @@ PAGE="""\
 <center><img src="stream.mjpg" width="640" height="480"></center>
 </body>
 <body>
-<button type="button" name="ForceArm" value="true"> Force Arm </button>
+<button type="submit" name="ForceArm" value="true"> Force Arm </button>
 </body>
 </html>
 """
