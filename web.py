9cc8fb48cd4f097280d96cabebe65d139f7be6c7
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-12 10:16:56 +0700

    testing

--- web.py
+++ web.py
@@ -46,37 +46,6 @@ class StreamingOutput(object):
                 self.condition.notify_all()
             self.buffer.seek(0)
         return self.buffer.write(buf)
-class StreamingHandler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            self.send_response(301)
-            self.send_header('Location', '/stream.mjpg')
-            self.end_headers()
-        if self.path.endswith('/stream.mjpg'):
-            self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-            self.end_headers()
-            try:
-                while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
-                    self.end_headers()
-                    self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
-            except Exception as e:
-                logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
-        else:
-            self.send_error(404)
-            self.end_headers()
 
 class WebServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
@@ -140,8 +109,6 @@ class webHandler(server.BaseHTTPRequestHandler):
         if self.path.find("ForceArm=true") != -1:
                 print("Run Force Arm")
                 test1.force_arm()
-                if(test1.arm == 0):
-                    output = 'alert("Cannot Force Arm")'
 
 with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
