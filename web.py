b358c07c605e0dd52666c77b363c61b6d511a5ff
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-12 09:07:03 +0700

    testing

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, Response
 # Raspberry Pi camera module (requires picamera package, developed by Miguel Grinberg)
 from camera_pi import Camera
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,14 +15,12 @@ def gen(camera):
     """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(Camera()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(gen(Camera()),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='', port =80, debug=True, threaded=True)
+    app.run(debug=True,port =8160)
