4175e993a4e2e12558a39a2ac837565a10e1b58a
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-12 09:37:48 +0700

    testing

--- web.py
+++ web.py
@@ -68,7 +68,6 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     self.send_header('Content-Type', 'image/jpeg')
                     self.send_header('Content-Length', len(frame))
                     self.end_headers()
-                    self.wfile.write(content)
                     self.wfile.write(frame)
                     self.wfile.write(b'\r\n')
             except Exception as e:
