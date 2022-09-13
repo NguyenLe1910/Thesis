cd8bf233f3c7e10a95e967b6e3f7ea2e1117eae4
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-13 17:22:59 +0700

    testing

--- test1.py
+++ test1.py
@@ -65,4 +65,5 @@ def setmode(mode):
 def msg_attitude():
     print("Heartbeat from system (system %u component %u)" % (master.target_system, master.target_component))
     msg_attitude = master.recv_match(type='ATTITUDE',blocking=True)
+    print(msg_attitude)
 
