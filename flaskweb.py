1f7008351cc18fcec9fc56ef81e28a1e2d299688
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-13 23:18:28 +0700

    testing

--- flaskweb.py
+++ flaskweb.py
@@ -8,17 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html') #you can customze index.html here
 
-def gen(camera):
-    #get camera frame
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(pi_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 if __name__ == '__main__':
