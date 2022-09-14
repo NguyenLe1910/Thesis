0ae5950aa4be5f3e91aaa3bb9653083d84b809bd
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-14 17:14:04 +0700

    testing

--- flaskweb.py
+++ flaskweb.py
@@ -19,14 +19,6 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-def stream_template(template_name, **context):
-    app.update_template_context(context)
-    t = app.jinja_env.get_template(template_name)
-    rv = t.stream(context)
-    # uncomment if you don't need immediate reaction
-    ##rv.enable_buffering(5)
-    return rv
-
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(pi_camera),
@@ -39,9 +31,9 @@ def conected():
         attitude = str(test1.msg_attitude())
         while True :
             attitude = str(test1.msg_attitude())
-            time.sleep(.2)  # an artificial delay
+            time.sleep(.01)  # an artificial delay
             yield attitude
-    return Response(stream_template('conected.html', data=g()))
+    return render_template('conected.html', attitude=msg_attitude)
 
 @app.route('/Arming')
 def arming():
@@ -54,16 +46,23 @@ def disarm():
     msg_attitude = str(test1.msg_attitude())
     render_template('conected.html', attitude=msg_attitude)
 
+def stream_template(template_name, **context):
+    app.update_template_context(context)
+    t = app.jinja_env.get_template(template_name)
+    rv = t.stream(context)
+    # uncomment if you don't need immediate reaction
+    ##rv.enable_buffering(5)
+    return rv
 
-@app.route('/testxxx')
-def testxxx():
+@app.route('/sys_status_stream')
+def sys_status_stream():
     def g():
         attitude = str(test1.msg_attitude())
         while True :
             attitude = str(test1.msg_attitude())
             time.sleep(.01)  # an artificial delay
             yield attitude
-    return Response(stream_template('test.html', data=g()))
-    
+    return Response(stream_template('sys_status_stream.html', data=g()))
+
 if __name__ == '__main__':
     app.run(host='192.168.63.12', port=8000)
