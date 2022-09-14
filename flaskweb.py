689bc94d36976b7060d1d6c56dd3968d0cbc959f
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-14 17:18:33 +0700

    testing

--- flaskweb.py
+++ flaskweb.py
@@ -33,7 +33,7 @@ def conected():
             attitude = str(test1.msg_attitude())
             time.sleep(.01)  # an artificial delay
             yield attitude
-    return render_template('conected.html', attitude = g())
+    return render_template('conected.html', attitude = attitude in g() )
 
 @app.route('/Arming')
 def arming():
