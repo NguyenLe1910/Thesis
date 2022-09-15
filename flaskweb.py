b645ed93ff5f9f9e49d0741044e6e06faca2f326
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-16 02:23:29 +0700

    testing

--- flaskweb.py
+++ flaskweb.py
@@ -19,39 +19,6 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(pi_camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/Connected')
-def conected():
-    test1.wait_conn()
-    return render_template('connected.html')
-
-@app.route('/Arming')
-def arming():
-    #test1.force_arm()
-    msg_attitude = str(test1.msg_attitude())
-    return render_template('arming.html', attitude=msg_attitude)
-
-@app.route('/Disarm')
-def disarm():
-    msg_attitude = str(test1.msg_attitude())
-    render_template('conected.html', attitude=msg_attitude)
-
-@app.route('/xx')
-def xx():
-    def g():
-        attitude = str(test1.msg_attitude())
-        while True :
-            attitude = str(test1.msg_attitude())
-            time.sleep(.01)  # an artificial delay
-            yield attitude
-    return Response(stream_template('xx.html', data=g()))
-
-
-
 def stream_template(template_name, **context):
     app.update_template_context(context)
     t = app.jinja_env.get_template(template_name)
@@ -59,6 +26,65 @@ def stream_template(template_name, **context):
     # uncomment if you don't need immediate reaction
     ##rv.enable_buffering(5)
     return rv
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(pi_camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/Connected')
+def conected():
+    def g():
+        while True :
+            attitude = str(test1.msg_attitude())
+            roll_position=attitude.find('roll')
+            pitch_position=attitude.find('pitch')
+            yaw_position=attitude.find('yaw')
+            rollspeed_position=attitude.find('rollspeed')
+
+            roll = float(attitude[roll_position+7:pitch_position-2])
+            pitch = float(attitude[pitch_position+8:yaw_position-2])
+            yaw = float(attitude[yaw_position+6:rollspeed_position-2])
+            yield roll,pitch,yaw
+    return Response(stream_template('connected.html', data=g()))
+
+
+@app.route('/Arming')
+def arming():
+    #test1.force_arm()
+    def g():
+        while True :
+            attitude = str(test1.msg_attitude())
+            roll_position=attitude.find('roll')
+            pitch_position=attitude.find('pitch')
+            yaw_position=attitude.find('yaw')
+            rollspeed_position=attitude.find('rollspeed')
+
+            roll = float(attitude[roll_position+7:pitch_position-2])
+            pitch = float(attitude[pitch_position+8:yaw_position-2])
+            yaw = float(attitude[yaw_position+6:rollspeed_position-2])
+            yield roll,pitch,yaw
+    return Response(stream_template('arming.html', data=g()))
+
+
+@app.route('/Disarm')
+def disarm():
+    def g():
+        while True :
+            attitude = str(test1.msg_attitude())
+            roll_position=attitude.find('roll')
+            pitch_position=attitude.find('pitch')
+            yaw_position=attitude.find('yaw')
+            rollspeed_position=attitude.find('rollspeed')
+
+            roll = float(attitude[roll_position+7:pitch_position-2])
+            pitch = float(attitude[pitch_position+8:yaw_position-2])
+            yaw = float(attitude[yaw_position+6:rollspeed_position-2])
+            yield roll,pitch,yaw
+    return Response(stream_template('connected.html', data=g()))
+
 
 @app.route('/sys_status_stream')
 def sys_status_stream():
@@ -75,22 +101,6 @@ def sys_status_stream():
             yaw = float(attitude[yaw_position+6:rollspeed_position-2])
             yield roll,pitch,yaw
     return Response(stream_template('sys_status_stream.html', data=g()))
-
-@app.route('/testing1')
-def testing1():
-    def g():
-        while True :
-            attitude = str(test1.msg_attitude())
-            roll_position=attitude.find('roll')
-            pitch_position=attitude.find('pitch')
-            yaw_position=attitude.find('yaw')
-            rollspeed_position=attitude.find('rollspeed')
-
-            roll = float(attitude[roll_position+7:pitch_position-2])
-            pitch = float(attitude[pitch_position+8:yaw_position-2])
-            yaw = float(attitude[yaw_position+6:rollspeed_position-2])
-            yield roll,pitch,yaw
-    return Response(stream_template('testing1.html', data=g()))
 
 if __name__ == '__main__':
     app.run(host='192.168.63.12', port=8000)
