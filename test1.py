6311c6b4daaf3fa5223e30c10f07867e091903a0
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-17 03:12:48 +0700

    testing

--- test1.py
+++ test1.py
@@ -63,5 +63,22 @@ def setmode(mode):
     ack_msg = master.recv_match(type='COMMAND_ACK',blocking=True)
     print(ack_msg)
 
+def set_rc_channel_pwm(channel_id, pwm=1500):
+    """ Set RC channel pwm value
+    Args:
+        channel_id (TYPE): Channel ID
+        pwm (int, optional): Channel pwm value 1100-1900
+    """
+    if channel_id < 1 or channel_id > 10:
+        print("Channel does not exist.")
+        return
+        
+    rc_channel_values = [65535 for _ in range(18)]
+    rc_channel_values[channel_id - 1] = pwm
+    master.mav.rc_channels_override_send(
+        master.target_system,                # target_system
+        master.target_component,             # target_component
+        *rc_channel_values)                  # RC channel list, in microseconds.
+
 def msg_attitude():
     return master.recv_match(type='ATTITUDE',blocking=True)
