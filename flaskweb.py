f9877236e5dfd3f4407b8d2f1ef57be489e181f1
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-18 19:53:17 +0700

    testing

--- flaskweb.py
+++ flaskweb.py
@@ -91,7 +91,11 @@ def disarm():
 
 @app.route('/sys_status_stream',methods =["GET","POST"])
 def sys_status_stream():
-
+    if request.method == "POST":
+        data = request.get_json()
+        print(data)
+    else :
+        print('GET')
     return Response(stream_template('sys_status_stream.html', data=sys_status_needed()))
  
 if __name__ == '__main__':
