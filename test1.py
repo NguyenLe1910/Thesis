b2c85450d4e7f2baecd6abb828ca3a58b3cb9db2
NguyenLe1910 <lexuannguyen001@gmail.com>
2022-09-19 11:03:04 +0700

    19/9

--- test1.py
+++ test1.py
@@ -66,4 +66,4 @@ def msg_attitude():
 def msg_GPS_RAW():
     return master.recv_match(type='GPS_RAW_INT',blocking=True)
 
-master.recv_match(type='SYS_STATUS',blocking=True)
+print(master.recv_match(type='SYS_STATUS',blocking=True))
